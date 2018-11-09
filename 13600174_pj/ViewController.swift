b27578e9bf67f842e5ae90986051a20d451e1f22
Ladapim Vongkedtrachon <ladapimvongkedtrachon@ladapims-macbook-pro.local>
2018-11-09 22:31:04 +0700

    Initial Commit

---
+++ 13600174_pj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  13600174_pj
+//
+//  Created by Ladapim Vongkedtrachon on 9/11/2561 BE.
+//  Copyright © 2561 ICTSUIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
